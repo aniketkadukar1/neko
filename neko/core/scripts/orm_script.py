07e6271bef1f7d21d6d1ad2216dea6e191bbb388
Aniket kadukar <aniketdkadukar@gmail.com>
2024-10-15 21:40:45 +0530

    Update orm_script.py

--- neko/core/scripts/orm_script.py
+++ neko/core/scripts/orm_script.py
@@ -2,13 +2,11 @@ from django.contrib.auth.models import User
 from core.models import Restaurent, Rating, Sales
 from django.db import connection
 from pprint import pprint
+from django.db.models import F
 
 def run():
-    restaurent = Restaurent.objects.select_related()
-    pprint(restaurent)
-    print("_________________________________________________")
-    
-
-    print("_________________________________________________")
-    
-    pprint(connection.queries)
+    rating = Rating.objects.filter(rating=3).first()
+    rating.rating = F('rating') + 1
+    rating.save()
+    rating.refresh_from_db()
+    print(rating.rating)
